44129a2dd866870f0ab0c861c005e5a395343b3c
Georg Schmucker <georg.schmucker@ski-online.de>
2026-06-05 23:59:04 +0200

    0.1.0-0000004

--- app/web/context_processors.py
+++ app/web/context_processors.py
@@ -3,7 +3,7 @@ from django.db import connection
 def versionnumber_processor(request):
     buildYear = 2026
     versionNumber = "0.1.0"
-    buildNumber = "0000003"
+    buildNumber = "0000004"
     return {'versionnumber': versionNumber,'buildNumber': buildNumber, 'buildYear':buildYear,}
 def datenbank_processor(request):
     db_name = connection.settings_dict['NAME']
